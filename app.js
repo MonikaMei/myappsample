b5ca2ff9edd99707de75fb22058d7803d6cd68d8
MonikaMei <33898455+MonikaMei@users.noreply.github.com>
2018-07-09 10:26:48 +0200

    added drag

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('Hello');
+var dropzone:
+function setup() {
+  createCanvas(200, 200);
+  background(0);
+
+  dropzone = select ('#dropzone');
+  dropzone.dragOver(highlight);
+  dropzone.dragLeave(unhighlight);
+}
+function highlight() {
+  dropsone.style('background-color','#ccc');
+}
+  function unhighlight() {
+  dropsone.style('background-color','#nnn');
+}
